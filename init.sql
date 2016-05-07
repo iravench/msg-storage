6f5f414afb086ac8ebdc4a8d85929a9f57ca095d
iravench <iravench@users.noreply.github.com>
2016-05-07 14:13:40 +1000

    remove fm registration from mysql

--- init.sql
+++ init.sql
@@ -1,14 +1,8 @@
-drop table if exists session, fm_registration;
+drop table if exists session;
 
 CREATE TABLE session (
   id MEDIUMINT NOT NULL AUTO_INCREMENT,
   user_id VARCHAR(30) NOT NULL,
   device_id VARCHAR(50) NOT NULL,
   policy VARCHAR(50),
-  PRIMARY KEY (id));
-
-CREATE TABLE fm_registration (
-  id VARCHAR(10) NOT NULL,
-  ip VARCHAR(30) NOT NULL,
-  port VARCHAR(10) NOT NULL,
   PRIMARY KEY (id));
